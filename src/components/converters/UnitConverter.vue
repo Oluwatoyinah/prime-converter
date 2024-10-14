f4829884d905b4ca68e4ffaaf1916f2fbe83e7cd
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2024-10-14 17:27:41 +0100

    feat: conversion type, groups, ratios constants added

--- src/components/converters/UnitConverter.vue
+++ src/components/converters/UnitConverter.vue
@@ -1,5 +1,38 @@
+<script setup>
+import { UNIT_CONVERSION_GROUP, UNIT_CONVERSION_TYPES } from '@/constants'
+import { ref } from 'vue'
+import SelectInput from '../form/SelectInput.vue'
+
+const unitGroup = ref('')
+const conversionType = ref('')
+</script>
+
 <template>
   <div class="p-2">
-    <p class="text-[13px]">Unit Converter</p>
+    <div>
+      <h3
+        class="bg-green-200 w-fit p-2 px-3 rounded-full text-[14px] font-semibold text-neutral-600"
+      >
+        Unit Conversion
+      </h3>
+    </div>
+
+    <div class="px-5">
+      <div class="mt-5">
+        <SelectInput
+          label="What type of unit conversion do you want?"
+          v-model="unitGroup"
+          :options="UNIT_CONVERSION_TYPES"
+        />
+      </div>
+
+      <div v-if="unitGroup" class="mt-5">
+        <SelectInput
+          label="Conversion Type"
+          v-model="conversionType"
+          :options="UNIT_CONVERSION_GROUP[unitGroup]"
+        />
+      </div>
+    </div>
   </div>
 </template>
--- src/constants/index.js
+++ src/constants/index.js
@@ -12,3 +12,142 @@ export const CONVERSION_OPTIONS = [
     value: 'language',
   },
 ]
+
+export const UNIT_CONVERSION_TYPES = [
+  {
+    name: 'Length',
+    value: 'length',
+  },
+  {
+    name: 'Area',
+    value: 'area',
+  },
+  {
+    name: 'Volume',
+    value: 'volume',
+  },
+  {
+    name: 'Weight',
+    value: 'weight',
+  },
+  {
+    name: 'Time',
+    value: 'time',
+  },
+
+  {
+    name: 'Temperature',
+    value: 'temperature',
+  },
+  {
+    name: 'Speed',
+    value: 'speed',
+  },
+]
+
+export const UNIT_CONVERSION_GROUP = {
+  length: [
+    { name: 'Inches to Centimeters', value: 'inches_to_centimeters' },
+    { name: 'Feet to Meters', value: 'feet_to_meters' },
+    { name: 'Miles to Kilometers', value: 'miles_to_kilometers' },
+    { name: 'Yards to Meters', value: 'yards_to_meters' },
+    { name: 'Millimeters to Inches', value: 'millimeters_to_inches' },
+  ],
+
+  area: [
+    {
+      name: 'Square Feet to Square Meters',
+      value: 'square_feet_to_square_meters',
+    },
+    {
+      name: 'Square Meters to Square Yards',
+      value: 'square_meters_to_square_yards',
+    },
+    { name: 'Acres to Square Meters', value: 'acres_to_square_meters' },
+    {
+      name: 'Square Miles to Square Kilometers',
+      value: 'square_miles_to_square_kilometers',
+    },
+    {
+      name: 'Square Inches to Square Centimeters',
+      value: 'square_inches_to_square_centimeters',
+    },
+  ],
+
+  volume: [
+    { name: 'Liters to Milliliters', value: 'liters_to_milliliters' },
+    { name: 'Gallons (US) to Liters', value: 'gallons_us_to_liters' },
+    { name: 'Cubic Feet to Cubic Meters', value: 'cubic_feet_to_cubic_meters' },
+    {
+      name: 'Cubic Inches to Cubic Centimeters',
+      value: 'cubic_inches_to_cubic_centimeters',
+    },
+    { name: 'Quarts (US) to Liters', value: 'quarts_us_to_liters' },
+  ],
+
+  weight: [
+    { name: 'Pounds to Kilograms', value: 'pounds_to_kilograms' },
+    { name: 'Ounces to Grams', value: 'ounces_to_grams' },
+    { name: 'Tons (US) to Kilograms', value: 'tons_us_to_kilograms' },
+    { name: 'Kilograms to Pounds', value: 'kilograms_to_pounds' },
+    { name: 'Grams to Milligrams', value: 'grams_to_milligrams' },
+  ],
+
+  time: [
+    { name: 'Seconds to Milliseconds', value: 'seconds_to_milliseconds' },
+    { name: 'Minutes to Seconds', value: 'minutes_to_seconds' },
+    { name: 'Hours to Minutes', value: 'hours_to_minutes' },
+    { name: 'Days to Hours', value: 'days_to_hours' },
+    { name: 'Weeks to Days', value: 'weeks_to_days' },
+  ],
+
+  temperature: [
+    { name: 'Celsius to Fahrenheit', value: 'celsius_to_fahrenheit' },
+    { name: 'Fahrenheit to Celsius', value: 'fahrenheit_to_celsius' },
+    { name: 'Kelvin to Celsius', value: 'kelvin_to_celsius' },
+    { name: 'Celsius to Kelvin', value: 'celsius_to_kelvin' },
+  ],
+
+  speed: [
+    { name: 'Miles per Hour to Kilometers per Hour', value: 'mph_to_kph' },
+  ],
+}
+
+const CONVERSION_RATIO = {
+  inches_to_centimeters: 2.54,
+  feet_to_meters: 0.3048,
+  miles_to_kilometers: 1.60934,
+  yards_to_meters: 0.9144,
+  millimeters_to_inches: 0.0393701,
+
+  square_feet_to_square_meters: 0.092903,
+  square_meters_to_square_yards: 1.19599,
+  acres_to_square_meters: 4046.86,
+  square_miles_to_square_kilometers: 2.58999,
+  square_inches_to_square_centimeters: 6.4516,
+
+  liters_to_milliliters: 1000,
+  gallons_us_to_liters: 3.78541,
+  cubic_feet_to_cubic_meters: 0.0283168,
+  cubic_inches_to_cubic_centimeters: 16.3871,
+  quarts_us_to_liters: 0.946353,
+
+  pounds_to_kilograms: 0.453592,
+  ounces_to_grams: 28.3495,
+  tons_us_to_kilograms: 907.185,
+  kilograms_to_pounds: 2.20462,
+  grams_to_milligrams: 1000,
+
+  seconds_to_milliseconds: 1000,
+  minutes_to_seconds: 60,
+  hours_to_minutes: 60,
+  days_to_hours: 24,
+  weeks_to_days: 7,
+
+  celsius_to_fahrenheit: 'C × 9/5 + 32', // Formula
+  fahrenheit_to_celsius: '(F - 32) × 5/9', // Formula
+  kelvin_to_celsius: 'K - 273.15', // Formula
+  celsius_to_kelvin: 'C + 273.15', // Formula
+
+  mph_to_kph: 1.60934,
+}
